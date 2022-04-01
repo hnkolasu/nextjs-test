28bb6661484c99884dd95c2874e0580846e0b9cc
Nicolas Quiering <nicolasquiering@gmail.com>
2022-04-01 16:24:19 -0300

    Added meta tags

--- pages/landing.jsx
+++ pages/landing.jsx
@@ -1,42 +1,42 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
-export default function Home () {
+export default function Home() {
   return (
     <div>
       <Head>
 
         {/* <!-- HTML Meta Tags --> */}
-        <title>Lean it 101 - Agile & lean business transformation</title>
-        <meta name='title' content='Lean it 101 - Agile & lean business transformation' />
+        <title>Lean Agile Foundation</title>
+        <meta name='title' content='Lean Agile Foundation' />
         <meta
           name='description'
-          content="We maximize our customer's value delivery by developing people, methods and technologies to accelerate lean agile transformations."
+          content='Aprenda toda a essência do Lean e do Agile para transformar a sua organização e entregar mais valor para os seus clientes em menos tempo'
         />
 
         {/* <!-- Open Graph / Facebook --/> */}
         <meta property='og:type' content='website' />
         <meta property='og:url' content='https://leanit101.com/' />
-        <meta property='og:title' content='Lean it 101 - Agile & lean business transformation' />
+        <meta property='og:title' content='Lean Agile Foundation' />
         <meta
           property='og:description'
-          content="We maximize our customer's value delivery by developing people, methods and technologies to accelerate lean agile transformations."
+          content='Aprenda toda a essência do Lean e do Agile para transformar a sua organização e entregar mais valor para os seus clientes em menos tempo'
         />
-        <meta property='og:image' content='https://leanit101.com/images/socialMediaHpV3.jpg' />
+        <meta property='og:image' content='https://leanit101.com/images/socialMediaLp.jpg' />
 
         {/* <!-- Twitter --/> */}
         <meta property='twitter:card' content='summary_large_image' />
         <meta property='twitter:url' content='https://leanit101.com/' />
-        <meta property='twitter:title' content='Lean it 101 - Agile & lean business transformation' />
+        <meta property='twitter:title' content='Lean Agile Foundation' />
         <meta
           property='twitter:description'
-          content="We maximize our customer's value delivery by developing people, methods and technologies to accelerate lean agile transformations."
+          content='Aprenda toda a essência do Lean e do Agile para transformar a sua organização e entregar mais valor para os seus clientes em menos tempo'
         />
-        <meta property='twitter:image' content='https://leanit101.com/images/socialMediaHpV3.jpg' />
+        <meta property='twitter:image' content='https://leanit101.com/images/socialMediaLp.jpg' />
 
       </Head>
-      <Link href='/landing'>
-        <h1>index</h1>
+      <Link href='/'>
+        <h1>landing</h1>
       </Link>
     </div>
   )
